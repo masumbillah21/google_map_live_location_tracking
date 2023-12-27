04d8ead2e281fcf34fff5991555db2ac4aac02b2
H. M. Masum Billah <mbillah21@gmail.com>
2023-12-27 19:25:50 +0600

    Real Time Tracking On google map completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,56 +15,15 @@ class _HomeScreenState extends State<HomeScreen> {
   GoogleMapController? mapController;
   LocationData? currentLocation;
   Set<LatLng> polylinePoints = <LatLng>{};
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map Animation and Tracking'),
-      ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(23.808397346705693,
-              90.41005799898663), // Default initial position
-          zoom: 12.0,
-        ),
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        polylines: {
-          Polyline(
-            polylineId: const PolylineId('userRoute'),
-            points: polylinePoints.toList(),
-            color: Colors.blue,
-            width: 5,
-          ),
-        },
-        markers: {
-          Marker(
-            markerId: const MarkerId('userMarker'),
-            position: LatLng(
-              currentLocation?.latitude ?? 23.808397346705693,
-              currentLocation?.longitude ?? 90.41005799898663,
-            ),
-            onTap: () {
-              _showInfoWindow();
-            },
-          ),
-        },
-        mapType: MapType.terrain,
-      ),
-    );
-  }
+  bool _isOpenInfoWindow = false;
 
   void _onMapCreated(GoogleMapController controller) {
     setState(() {
       mapController = controller;
     });
 
-    // Fetch user's initial location
     _fetchLocation();
 
-    // Fetch location every 10 seconds
     Timer.periodic(const Duration(seconds: 10), (timer) {
       _fetchLocation();
     });
@@ -94,9 +53,64 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       currentLocation = locationData;
     });
+    if (_isOpenInfoWindow) {
+      _showInfoWindow();
+    }
+  }
+
+  InfoWindow infoWindow() {
+    return InfoWindow(
+      title: 'My current location',
+      snippet:
+          'Lat: ${currentLocation?.latitude?.toStringAsFixed(6) ?? 23.808397346705693}, Lng: ${currentLocation?.longitude?.toStringAsFixed(6) ?? 90.41005799898663}',
+    );
   }
 
   void _showInfoWindow() {
     mapController?.showMarkerInfoWindow(const MarkerId('userMarker'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Real-Time Location Tracker'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(23.808397346705693,
+              90.41005799898663), // Default initial position
+          zoom: 12.0,
+        ),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        polylines: {
+          Polyline(
+            polylineId: const PolylineId('userRoute'),
+            points: polylinePoints.toList(),
+            color: Colors.blue,
+            width: 5,
+          ),
+        },
+        markers: {
+          Marker(
+            markerId: const MarkerId('userMarker'),
+            position: LatLng(
+              currentLocation?.latitude ?? 23.808397346705693,
+              currentLocation?.longitude ?? 90.41005799898663,
+            ),
+            infoWindow: infoWindow(),
+            onTap: () {
+              setState(() {
+                _isOpenInfoWindow = true;
+              });
+              _showInfoWindow();
+            },
+          ),
+        },
+        mapType: MapType.terrain,
+      ),
+    );
   }
 }
